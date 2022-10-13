b68a4e947376403cbfb72a020c6d8562c42cdd4a
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-10-13 16:28:54 +0300

    Add serializers

---
+++ Assets/Scripts/Serializers/AnimationCurveSerializer.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using Mirror;
+using UnityEngine;
+
+[UsedImplicitly(ImplicitUseTargetFlags.Members)]
+public static class AnimationCurveSerializer
+{
+    public static void WriteAnimationCurve(this NetworkWriter writer, AnimationCurve curve)
+    {
+        writer.Write((int)curve.preWrapMode);
+        writer.Write((int)curve.postWrapMode);
+        writer.Write(curve.keys.Length);
+        foreach (Keyframe key in curve.keys)
+        {
+            writer.Write(key);
+        }
+    }
+
+    public static AnimationCurve ReadAnimationCurve(this NetworkReader reader)
+    {
+        var preWrapMode = (WrapMode)reader.Read<int>();
+        var postWrapMode = (WrapMode)reader.Read<int>();
+        int length = reader.Read<int>();
+        var keys = new Keyframe[length];
+        for (int i = 0; i < length; i++)
+        {
+            keys[i] = reader.Read<Keyframe>();
+        }
+
+        var curve = new AnimationCurve(keys)
+        {
+            preWrapMode = preWrapMode,
+            postWrapMode = postWrapMode,
+        };
+        return curve;
+    }
+}
---
+++ Assets/Scripts/Serializers/KeyframeSerializer.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using Mirror;
+using UnityEngine;
+
+[UsedImplicitly(ImplicitUseTargetFlags.Members)]
+public static class KeyframeSerializer
+{
+    public static void WriteKeyframe(this NetworkWriter writer, Keyframe keyFrame)
+    {
+        writer.Write(keyFrame.time);
+        writer.Write(keyFrame.value);
+        writer.Write(keyFrame.inTangent);
+        writer.Write(keyFrame.outTangent);
+        writer.Write(keyFrame.inWeight);
+        writer.Write(keyFrame.outWeight);
+        writer.Write((int)keyFrame.weightedMode);
+    }
+
+    public static Keyframe ReadKeyframe(this NetworkReader reader)
+    {
+        float time = reader.Read<float>();
+        float value = reader.Read<float>();
+        float inTangent = reader.Read<float>();
+        float outTangent = reader.Read<float>();
+        float inWeight = reader.Read<float>();
+        float outWeight = reader.Read<float>();
+        return new Keyframe(time, value, inTangent, outTangent, inWeight, outWeight);
+    }
+}

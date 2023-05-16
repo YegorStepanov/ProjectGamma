ecce047139660a7be1d15115bcdbe8e6ba24c8ca
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:24:03 +0300

    Move ConstructPlayer to GameFactory

--- Assets/Scripts/Infrastructure/GameFactory.cs
+++ Assets/Scripts/Infrastructure/GameFactory.cs
@@ -5,12 +5,24 @@ namespace Infrastructure
 {
     public sealed class GameFactory : MonoBehaviour
     {
+        [SerializeField] private Camera _camera;
         [SerializeField] private CameraController _playerCameraPrefab;
+        [SerializeField] private PlayerSettings _playerSettings;
 
-        public CameraController CreateCamera(Transform focusOn)
+        public void ConstructPlayer(GameObject playerGameObject)
+        {
+            Player player = playerGameObject.GetComponent<Player>().NotNull();
+
+            CameraController cameraController = CreateCamera(player.CameraFocusPoint);
+            var inputManager = new RelativeInputManagerDecorator(new DefaultInputManager(), cameraController.transform);
+            player.Construct(_playerSettings, inputManager);
+        }
+
+        private CameraController CreateCamera(Transform focusOn)
         {
             CameraController controller = Instantiate(_playerCameraPrefab);
-            controller.Construct(Camera.main, new InputManager());
+            var inputManager = new DefaultInputManager();
+            controller.Construct(_camera, inputManager);
             controller.FocusOn = focusOn;
             return controller;
         }

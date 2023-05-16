f94706697be3696c450568f0adef9bb9089700df
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:26:23 +0300

    Rework

--- Assets/Scripts/Infrastructure/Server/ServerRoomManager.cs
+++ Assets/Scripts/Infrastructure/Server/ServerRoomManager.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 using Mirror;
-using Mirror.SimpleWeb;
 using Room;
 using UnityEngine;
 
@@ -10,20 +9,13 @@ namespace Infrastructure.Server
     public sealed class ServerRoomManager : NetworkBehaviour
     {
         [SerializeField] private ClientRoomManager _clientRoomManager;
-        [SerializeField] private PlayerSettings _playerSettings;
         [SerializeField] private ServerHeroCollisionManager _serverHeroCollisionManager;
+        [SerializeField] private GameFactory _gameFactory;
 
         private FreeStartPositions _freeStartPositions;
         private int _botCounts;
 
         public RoomPlayers RoomPlayers { get; private set; }
-
-        private void Update()
-        {
-            if (RoomPlayers?.Players?.Count != 2)
-                return;
-            // Debug.Log($"{RoomPlayers.Players.Count} ; {RoomPlayers.Players[1].Position}");
-        }
 
         public void InitRoom(List<Transform> startPositions, PlayerSpawnMethod playerSpawnMethod)
         {
@@ -32,79 +24,74 @@ namespace Infrastructure.Server
             _botCounts = 0;
         }
 
+        [Server]
+        public Player CreatePlayer(Player playerPrefab)
+        {
+            Player player = Instantiate(playerPrefab);
+            RoomPlayers.AddPlayer(player);
+            _clientRoomManager.AddPlayerData(player);
+            return player;
+        }
+
+        [Server]
+        public void RemovePlayer(Player player)
+        {
+            RoomPlayers.RemovePlayer(player);
+            _clientRoomManager.RemovePlayerData(player);
+        }
+
+        [Server]
         public void ReplaceAndConstructPlayer(NetworkConnectionToClient conn, GameObject gamePlayer, int playerIndex)
         {
-            if (!isServer)
-            {
-                Debug.LogWarning("NOT SERVER!!!");
-            }
-
             Player player = gamePlayer.GetComponent<Player>().NotNull();
-            // player.Hit += PlayerOnHit;
-            if(isServer)
-                player.Collider.CollisionEntered += HandlePlayersCollision;
+            player.Collider.CollisionEntered += HandlePlayersCollisions;
 
-            Debug.Log($"subscribe to Hit: {player.Data.Name} {isServer} {isClient}");
-
-            PreparePlayerForGame(player, playerIndex);
+            SetUpPlayer(player, playerIndex);
             NetworkServer.ReplacePlayerForConnection(conn, gamePlayer, true);
 
-            _clientRoomManager.TargetConstructPlayer(conn, gamePlayer, _playerSettings);
+            TargetConstructPlayer(conn, gamePlayer);
         }
 
-        // Players
-        private void HandlePlayersCollision(Player player, Collider collidedBody)
-        {
-            // Debug.Log($"wtf collided {player.name} -> {collidedBody.name}");
-
-            if (!collidedBody.CompareTag(PlayerCollider.Tag)) //.transform?
-                return;
-
-            // Debug.Log($"CompareTag {player.name}");
-
-            if (collidedBody.TryGetComponent(out PlayerCollider collidedCollider))
-            {
-                // Debug.Log($"TryGetComponent {player.name}");
-                Player collidedPlayer = collidedCollider.Player;
-                _serverHeroCollisionManager.HandleColliderHit(player, collidedPlayer);
-            }
-        }
-
+        [Server]
         public void RestartGame()
-        {
-            PreparePlayersForGame();
-        }
-
-        private void PreparePlayersForGame()
         {
             _freeStartPositions.Reset();
             foreach (Player player in RoomPlayers.Players)
             {
                 Vector3 position = _freeStartPositions.Pop();
-                RoomPlayers.PreparePlayerToPlay(player, position, player.Data.Name);
+                SetInitialPlayerData(player, position, player.Data.Name);
+                TargetSetInitialPlayerData(player.connectionToClient, player, position, player.Data.Name);
             }
         }
 
-        private void PreparePlayerForGame(Player player, int playerIndex)
+        private void HandlePlayersCollisions(Player player, Collider collidedBody)
         {
-            //it works!
+            if (!collidedBody.CompareTag(PlayerCollider.Tag))
+                return;
+
+            if (collidedBody.TryGetComponent(out PlayerCollider collidedCollider))
+            {
+                Player collidedPlayer = collidedCollider.Player;
+                _serverHeroCollisionManager.HandleColliderHit(player, collidedPlayer);
+            }
+        }
+
+        private void SetUpPlayer(Player player, int playerIndex)
+        {
             Vector3 position = _freeStartPositions.Pop();
             string playerName = $"Player {playerIndex + 1}";
-            RoomPlayers.PreparePlayerToPlay(player, position, playerName);
+            SetInitialPlayerData(player, position, playerName);
         }
 
-        public Player CreatePlayer(Player playerPrefab)
-        {
-            Player player = Instantiate(playerPrefab);
-            RoomPlayers.AddPlayer(player);
-            _clientRoomManager.AddPlayer(player);
-            return player;
-        }
+        private void SetInitialPlayerData(Player player, Vector3 position, string playerName) =>
+            RoomPlayers.SetInitialPlayerData(player, position, playerName);
 
-        public void RemovePlayer(Player player)
-        {
-            RoomPlayers.RemovePlayer(player);
-            _clientRoomManager.RemovePlayer(player);
-        }
+        [TargetRpc]
+        private void TargetSetInitialPlayerData([UsedImplicitly] NetworkConnection target, Player player, Vector3 position, string playerName) =>
+            RoomPlayers.SetInitialPlayerData(player, position, playerName);
+
+        [TargetRpc]
+        private void TargetConstructPlayer([UsedImplicitly] NetworkConnection conn, GameObject playerGameObject) =>
+            _gameFactory.ConstructPlayer(playerGameObject);
     }
 }

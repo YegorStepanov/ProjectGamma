f601f19fc85aba5791f38f6b3a710224b2e4f67d
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:14:58 +0300

    Rename

---
+++ Assets/Scripts/Components/DestroyOnRestartByName.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public sealed class DestroyOnRestartByName : MonoBehaviour
+{
+    private bool _isCreated;
+
+    // Mirror loads the bootstrap scene on each client disconnect, so remove the old Managers to preserve component links
+    private void Awake()
+    {
+        foreach (var duplicateComponent in FindObjectsOfType<DestroyOnRestartByName>(true))
+        {
+            if (duplicateComponent.name != name) // todo
+                continue;
+
+            if (duplicateComponent._isCreated)
+            {
+                Debug.Log($"Destroying {name}"); // todo
+                Destroy(duplicateComponent.gameObject);
+            }
+        }
+
+        _isCreated = true;
+    }
+}

127fabddadf7a91bb77e7cb74dbfa6e8649784c9
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:19:47 +0300

    Rename

--- Assets/Scripts/Player/PlayerColor.cs
+++ Assets/Scripts/Player/PlayerColor.cs
@@ -10,24 +10,21 @@ public sealed class PlayerColor : MonoBehaviour
     private void Start()
     {
         _player = GetComponent<Player>().NotNull();
-        _player.Data.Changed += OnColorChanged;
+        _player.Data.ColorChanged += OnColorChanged;
 
         _material = CloneMaterial(_renderer);
     }
 
     private void OnDestroy()
     {
-        _player.Data.Changed -= OnColorChanged;
+        _player.Data.ColorChanged -= OnColorChanged;
 
         Destroy(_material);
     }
 
+    private void OnColorChanged(Color32 color) =>
+        _material.color = color;
+
     private static Material CloneMaterial(Renderer renderer) =>
         renderer.material;
-
-    private void OnColorChanged(Player player)
-    {
-        Debug.Log($"change color {player.Data.Color}");
-        _material.color = player.Data.Color;
-    }
 }

ab942f90bdf632accd77ef58c67a86c47991b3fd
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:26:00 +0300

    Rename

--- Assets/Scripts/Infrastructure/ClientRoomManager.cs
+++ Assets/Scripts/Infrastructure/ClientRoomManager.cs
@@ -1,40 +1,21 @@
-﻿using InputManagers;
-using JetBrains.Annotations;
-using Mirror;
-using UnityEngine;
+﻿using Mirror;
 
 namespace Infrastructure
 {
     public sealed class ClientRoomManager : NetworkBehaviour
     {
-        [SerializeField] public GameFactory _gameFactory;
-
         public readonly SyncList<PlayerData> PlayerDatas = new SyncList<PlayerData>();
 
         [Server]
-        public void AddPlayer(Player player)
+        public void AddPlayerData(Player player)
         {
             PlayerDatas.Add(player.Data);
         }
 
         [Server]
-        public void RemovePlayer(Player player)
+        public void RemovePlayerData(Player player)
         {
             PlayerDatas.Remove(player.Data);
-        }
-
-        [TargetRpc]
-        public void TargetConstructPlayer([UsedImplicitly] NetworkConnection conn, GameObject gamePlayer, PlayerSettings settings)
-        {
-            Player player = gamePlayer.GetComponent<Player>().NotNull();
-
-            CameraController cameraController = _gameFactory.CreateCamera(player.CameraFocusPoint);
-            var inputManager = new TransformRelativeInputManager(cameraController.transform);
-            player.Construct(settings, inputManager);
-
-            //todo: the state should be synced
-            //if (isLocalPlayer) //?
-            //    player.StateMachine.SetState(PlayerState.Walk);
         }
     }
 }

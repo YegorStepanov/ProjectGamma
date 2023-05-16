33d2d670a640d12b2a143bbcaea835e335d47e4a
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-16 11:15:13 +0300

    Add comment

--- Assets/Scripts/Components/DontDestroyOnLoad.cs
+++ Assets/Scripts/Components/DontDestroyOnLoad.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public sealed class DontDestroyOnLoad : MonoBehaviour
 {
+    // We cannot set the flag in Awake because it moves a gameobject to another scene (Mirror has not initialized yet), so it throws an exception when the client tries to connect:
+    // "Spawn scene object not found for [GUID]. Make sure that client and server use exactly the same project. This only happens if the hierarchy gets out of sync."
     private void Start()
     {
         DontDestroyOnLoad(this);

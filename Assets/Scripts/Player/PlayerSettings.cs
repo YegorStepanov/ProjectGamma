2132f2e172c3f19e8d3d393f75de1dfeaffb72f1
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-21 23:26:36 +0300

    Refactor SO to private fields with public properties

--- Assets/Scripts/Player/PlayerSettings.cs
+++ Assets/Scripts/Player/PlayerSettings.cs
@@ -4,20 +4,32 @@
 public sealed class PlayerSettings : ScriptableObject
 {
     [Range(0.01f, 30f)]
-    public float HorizontalRotationSpeedRadians = 1f;
+    [SerializeField] private float _horizontalRotationSpeedRadians = 25f;
     [Range(0.01f, 30f)]
-    public float VerticalRotationSpeedRadians = 5f;
+    [SerializeField] private float _verticalRotationSpeedRadians = 2f;
     [Min(0f)]
-    public float MinMoveDistance = 0.01f;
+    [SerializeField] private float _moveThreshold = 0.01f;
 
     [Header("Walking")]
     [Range(0.1f, 100f)]
-    public float WalkSpeed = 10f;
+    [SerializeField] private float _walkSpeed = 4f;
+    [SerializeField] private float _jumpHeight = 1.2f;
+    [SerializeField] private float _gravity = -15f;
 
     [Header("Dashing")]
     [Min(0.1f)]
-    public float DashDistance = 5f;
+    [SerializeField] private float _dashDistance = 5f;
     [Range(0.1f, 100f)]
-    public float DashMaxSpeed = 30f;
-    public AnimationCurve DashSpeedPercentage;
+    [SerializeField] private float _dashMaxSpeed = 14f;
+    [SerializeField] private AnimationCurve _dashSpeedPercentage;
+
+    public float HorizontalRotationSpeedRadians => _horizontalRotationSpeedRadians;
+    public float VerticalRotationSpeedRadians => _verticalRotationSpeedRadians;
+    public float MoveThreshold => _moveThreshold;
+    public float WalkSpeed => _walkSpeed;
+    public float DashDistance => _dashDistance;
+    public float DashMaxSpeed => _dashMaxSpeed;
+    public AnimationCurve DashSpeedPercentage => _dashSpeedPercentage;
+    public float JumpHeight => _jumpHeight;
+    public float Gravity => _gravity;
 }

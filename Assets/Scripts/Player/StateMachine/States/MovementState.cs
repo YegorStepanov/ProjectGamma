5efcac039f4b73e0db570ac640a6ac9f4a490b55
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-21 23:25:52 +0300

    Remove unused class

--- Assets/Scripts/Player/StateMachine/States/MovementState.cs
+++ Assets/Scripts/Player/StateMachine/States/MovementState.cs
@@ -13,26 +13,39 @@ public class MovementState : IPlayerState
         _functions = functions;
     }
 
-    public void Enter() { }
+    public void Enter()
+    {
+        _verticalVelocity = 0f;
+    }
 
     public void Exit() { }
 
     public void Update()
     {
+        if (_player.IsMovementBlocked)
+            return;
+
         if (_functions.IsDashPressed())
         {
             _player.StateMachine.SetState(PlayerState.Dash);
             return;
         }
 
-        if (_player.IsGrounded)
-            _gravityAcceleration = Vector3.zero;
-        else
-            _gravityAcceleration += Physics.gravity * Time.deltaTime;
+        //add terminalGravityVelocity
+        if (_functions.IsJumpPressed())
+        {
+            _verticalVelocity = Mathf.Sqrt(_player.Settings.JumpHeight * -2f * _player.Settings.Gravity);
+
+            // if (_jumpTimeoutDelta >= 0.0f)
+            //     _jumpTimeoutDelta -= Time.deltaTime;
+        }
+
+        if (!_player.IsGrounded)
+            _verticalVelocity += _player.Settings.Gravity * Time.deltaTime; //todo
 
         Vector3 moveDirection = _functions.GetMovementDirection();
 
-        _functions.Move(moveDirection, _player.Settings.WalkSpeed, _gravityAcceleration);
+        _functions.Move(moveDirection, _player.Settings.WalkSpeed, _verticalVelocity);
         _player.Animator.UpdateMovementAnimation(moveDirection);
     }
 }

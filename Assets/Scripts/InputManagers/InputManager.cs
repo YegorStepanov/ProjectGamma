6c2e2dca64fcdf856c26cfccb60725771049a724
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-24 10:00:44 +0300

    Revert "Remove left mouse click for dashing"
    This reverts commit 350efb2b916e77a8dac970ee27fa12ebe0df90cc.

--- Assets/Scripts/InputManagers/InputManager.cs
+++ Assets/Scripts/InputManagers/InputManager.cs
@@ -23,7 +23,7 @@ namespace InputManagers
 
         public bool ReadDashAction()
         {
-            return Input.GetKeyDown(KeyCode.LeftControl);
+            return Input.GetKeyDown(KeyCode.LeftControl) || Input.GetMouseButton(0);
         }
     }
 }
